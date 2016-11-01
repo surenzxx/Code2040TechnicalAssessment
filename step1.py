b9f38fd8079ae40530c63c6bb192192e8455c121
Surendra Persaud <persaudsurendra@gmail.com>
2016-11-01 17:58:35 -0400

    Delete step1.py

--- step1.py
+++
@@ -1,8 +0,0 @@
-import json
-import requests
-
-myToken = '7c9de6a9cba356ae4bbe6e86226cbbdf'
-myGithub= 'https://github.com/surenzxx/Code2040TechnicalAssessment'
-request = requests.post('http://challenge.code2040.org/api/register', json={'token':myToken,'github':myGit})
-
-print(request.text)
